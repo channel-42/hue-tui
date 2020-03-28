5722a200bf744826cb6fe4887647d8eda0308947
channel-42 <hallowmd@gmail.com>
2020-03-28 22:01:43 +0100

    Adding window for active lights and groups as well as login.json file with for user data

--- hue_tui.py
+++ hue_tui.py
@@ -1,48 +1,66 @@
 #!/bin/python3
 
 import sys
+import json
+import py_cui as cui
 sys.path.append('/path')
 from hue_snek import hue, Light
-import py_cui as cui
 
-h = hue('http://ip', 'username')
 
-#connection check
-if int(h.checkup()) == 1:
-    print("Error while connecting to the Hue API")
-    sys.exit(0)
+def login():
+    """login.
+    checks if userdate has been inserted into json file
+    """
+    with open("login.json") as f:
+        data = json.load(f)
+        ip = data["ip"]
+        user = data["user"]
+    if (ip != "") and (user != ""):
+        return data
+    else:
+        raise Exception("No IP and/or API user found")
+        return 1
 
 
 class HueTui:
     def __init__(self, master):
+        """__init__.
+
+        Args:
+            master: py_cui root module
+        """
         self.master = master
         self.scene = None
         self.bridge = []  #bridge info array
+        self.active = ""
 
         #add banner
         self.master.add_block_label(str(self.get_logo_text()), 0, 0)
 
         #items for each menu
-        self.lights = h.get_lights("name").values()
-        self.groups = h.get_groups("name").values()
-        self.scenes = h.get_scenes("name").values()
+        self.lights = H.get_lights("name").values()
+        self.groups = H.get_groups("name").values()
+        self.scenes = H.get_scenes("name").values()
 
         #add items to bridge array (dict -> array)
-        for param, val in h.get_bridge_info().items():
+        for param, val in H.get_bridge_info().items():
             self.bridge.append(f"{param}: {val}")
 
         #creating each menu
         self.lights_menu = self.master.add_scroll_menu("Lights", 1, 0, 2, 1)
         self.groups_menu = self.master.add_scroll_menu("Groups", 3, 0, 2, 1)
-        self.scenes_menu = self.master.add_scroll_menu("Scenes", 1, 1, 4, 2)
+        self.scenes_menu = self.master.add_scroll_menu("Scenes", 1, 1, 2, 1)
+        self.active_box = self.master.add_text_block("Active", 3, 1, 2, 1)
         self.bridge_information = self.master.add_scroll_menu(
-            "Hue Bridge", 0, 1, 1, 2)
+            "Hue Bridge", 0, 1, 1, 1)
 
         #adding items to each menu
         self.lights_menu.add_item_list(self.lights)
         self.groups_menu.add_item_list(self.groups)
         self.scenes_menu.add_item_list(self.scenes)
         self.bridge_information.add_item_list(self.bridge)
+
+        self.is_active()
 
         #adding keycommands
         self.lights_menu.add_key_command(cui.keys.KEY_ENTER, self.toggle_light)
@@ -51,28 +69,41 @@ class HueTui:
                                          command=self.scene_popup)
 
     def toggle_light(self):
+        """toggle_light.
+        Toggles a light on or off
+        """
         #toggle a light
-        states = h.get_lights()
+        states = H.get_lights()
         for light in states.items():
             if str(self.lights_menu.get()) == str(light[1].name):
                 if light[1].state == True:
-                    h.set_light(light[0], "on", "false")
+                    H.set_light(light[0], "on", "false")
                 else:
-                    h.set_light(light[0], "on", "true")
+                    H.set_light(light[0], "on", "true")
+
+        self.is_active()
 
     def toggle_group(self):
+        """toggle_group.
+        Toggle a entire group on or off
+        """
         #toggle a group
-        states = h.get_groups()
+        states = H.get_groups()
         for group in states.items():
             if str(self.groups_menu.get()) == str(group[1]["name"]):
                 if group[1]["action"]["on"] == True:
-                    h.set_group(group[0], "on", "false")
+                    H.set_group(group[0], "on", "false")
                 else:
-                    h.set_group(group[0], "on", "true")
+                    H.set_group(group[0], "on", "true")
+
+        self.is_active()
 
     def scene_popup(self):
+        """scene_popup.
+        Generates popup to select group to apply scene
+        """
         #main scene menu with popup to select which group is affected
-        groups = list(h.get_groups("name").values())
+        groups = list(H.get_groups("name").values())
 
         self.scene = str(self.scenes_menu.get())
 
@@ -81,16 +112,24 @@ class HueTui:
             self.enable_scene)
 
     def enable_scene(self, inp):
+        """enable_scene.
+
+        Args:
+            inp: the selected group from the popup menu
+        """
         #enable scene
         group = str(inp)
-        groups = h.get_groups("name")
+        groups = H.get_groups("name")
 
         if group in groups.values():
             group_id = list(groups.keys())[list(groups.values()).index(group)]
 
-            h.set_scene(group_id, self.scene)
+            H.set_scene(group_id, self.scene)
 
     def get_logo_text(self):
+        """get_logo_text.
+            makes the logo banner with linebreaks
+        """
         #make banner
 
         logo = '██╗  ██╗██╗   ██╗███████╗              ████████╗██╗   ██╗██╗\n'
@@ -102,8 +141,35 @@ class HueTui:
 
         return logo
 
+    def is_active(self):
+        """is_active.
+        checks what lights and groups are active and adds them to a string
+        """
+        self.active = "Lights:\n"
 
-root = cui.PyCUI(5, 5)
+        for ident, light in H.get_lights().items():
+            if light.state == True:
+                light = str(light.name)
+                self.active += f'{light}\n'
+
+        self.active += '\nGroups:\n'
+
+        for ident, value in H.get_groups().items():
+            if value["action"]["on"] == True:
+                group = str(H.get_group(int(ident), "name"))
+                self.active += f'{group}\n'
+
+        self.active_box.clear()
+        self.active_box.write(self.active)
+
+
+H = hue(login()["ip"], login()["user"])
+
+if int(H.checkup()) == 1:
+    print("Error while connecting to the Hue API")
+    sys.exit(0)
+
+root = cui.PyCUI(5, 2)
 root.set_title("Hue TUI")
 Main = HueTui(root)
 root.start()
