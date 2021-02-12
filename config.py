8df7631d661c56f00b8f477f4d674f6aa17ed7f8
channel-42 <info@devls.de>
2021-02-12 23:20:24 +0100

    Adding sample config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+"""
+SAMPLE CONFIG
+move or copy this file to ~/.config/hue-tui/
+"""
+import py_cui
+# general
+WPP = None
+UNICODE = True
+STEP_SIZE = 30
+# colors
+COLOR = py_cui.WHITE_ON_BLACK
+SELECTED_COLOR = py_cui.CYAN_ON_BLACK
+BORDER_COLOR = py_cui.BLUE_ON_BLACK
+LOGO_COLOR = py_cui.MAGENTA_ON_BLACK
+STATUSBAR_COLOR = py_cui.BLACK_ON_MAGENTA
+TITLEBAR_COLOR = py_cui.BLACK_ON_MAGENTA
+# dict for preset hue colors
+COLOR_DICT = {
+    "red": "#DE3838",
+    "blue": "#2122E2",
+    "green": "#2EF615",
+    "purple": "#5c0099",
+    "teal": "#26F0C9"
+}
