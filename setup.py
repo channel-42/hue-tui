dedce1cfb3ca68bfff43960125f9a7b5f6606849
Lukas <info@devls.de>
2024-02-28 20:02:56 +0100

    Updating to release 2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="huetui",
-    version="2.0",
+    version="2.1",
     author="channel-42",
     license="MIT",
     author_email="info@devls.de",
